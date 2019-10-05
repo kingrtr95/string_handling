4eb9a3001f292cd9bff9f108015bcd0a35839dd9
Rahul sharma <rahul.sharma1_cs18@gla.ac.in>
2019-10-05 23:49:40 +0530

    Initial commit

---
+++ src/main/Question_05.java
@@ -0,0 +1,15 @@
+package main;
+import java.util.Scanner;
+
+public class Question_05 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter string and sub string you want to replace ");
+        String str1 = obj.nextLine();
+        String str2 = obj.nextLine();
+        String str3 = obj.nextLine();
+        String newString = str1.replace(str2, str3);
+        System.out.println("new string: " + newString);
+
+    }
+}
